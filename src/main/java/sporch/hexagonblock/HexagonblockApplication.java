d3a7029bb7536f0f84bde5c7ba04319a48eee0e5
Yuanmeng Zeng <yz143@rice.edu>
2018-12-30 16:32:42 -0600

    create project

---
+++ src/main/java/sporch/hexagonblock/HexagonblockApplication.java
@@ -0,0 +1,14 @@
+package sporch.hexagonblock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication   // declare it is a spring boot application
+public class HexagonblockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HexagonblockApplication.class, args);
+    }
+
+}
+
