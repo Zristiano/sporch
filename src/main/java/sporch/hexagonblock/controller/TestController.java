d120cf5829b1c8fe5330bf33e0bd44250d12cd1b
Yuanmeng Zeng <yz143@rice.edu>
2019-01-01 22:21:02 -0600

    hello world

--- src/main/java/sporch/hexagonblock/controller/TestController.java
+++ src/main/java/sporch/hexagonblock/controller/TestController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
     @RequestMapping(value = "/",method = RequestMethod.GET,params = {"id","name"})
-    public String test(@RequestParam String id){
+    public String test(@RequestParam String id, @RequestParam String name){
         System.out.println("id->"+id);
-        return "hello world";
+        return "hello world" + "id->"+id+"   name->"+name +" timeStamp->"+System.currentTimeMillis()/1000;
     }
 }
